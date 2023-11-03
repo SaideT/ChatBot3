43ae4d52be0f910b9914e081e19386a63a1ad15c
Saide <50343932+SaideT@users.noreply.github.com>
2023-11-03 11:43:54 -0400

    only streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,5 @@
 import streamlit as st
-from hugchat import hugchat
-from hugchat.login import Login
 
 # App title
-st.set_page_config(page_title="HugChat2")
+st.set_page_config(page_title="Tax ChatBot")
 
