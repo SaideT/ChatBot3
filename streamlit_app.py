30d01c082a364f1eb3f136189e9a100bfaaf3934
Saide <50343932+SaideT@users.noreply.github.com>
2023-11-03 16:51:54 -0400

    change user to assistant

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ if "messages" not in st.session_state.keys():
 # User-provided prompt
 if prompt := st.chat_input(disabled=not openai_key):
     st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
+    with st.chat_message("assistant"):
         st.write(prompt)
 
 openai.api_key = openai_key
