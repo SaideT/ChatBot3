a8f37750959c16b794d3854a3d87911ef7f97990
Saide <50343932+SaideT@users.noreply.github.com>
2023-11-03 18:26:40 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,30 +16,38 @@ with st.sidebar:
  # Store chat messages, and initialize the chat message history
 
 if "messages" not in st.session_state.keys():
-    st.session_state.messages = [{"role": "assistant", "content": "Ask me a question"}]
+    st.session_state.messages = [{"role": "assistant", "content": "Ask me any questions"}]
+
+# Display the prior chat messages
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"]) 
 
 # User-provided prompt
 if prompt := st.chat_input(disabled=not openai_key):
     st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.write(prompt)
 
 # Function to get the GPT3.5's response
-def get_assistant_response(messages):
+def get_assistant_response(allmessages):
     r = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
-        messages=[{"role": m["role"], "content": m["content"]} for m in messages],
+        messages=[{"role": m["role"], "content": m["content"]} for m in allmessages],
     )
     response = r.choices[0].message.content
     return response
 
 if len(st.session_state.messages)>1:
-    response = get_assistant_response(st.session_state.messages)
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            response = generate_response(st.session_state.messages) 
+            st.write(response)
+
     newmessage = {"role": "assistant", "content": response}
     st.session_state.messages.append(newmessage)
 
-# Display the prior chat messages
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.write(message["content"])    
+   
 
 
 
